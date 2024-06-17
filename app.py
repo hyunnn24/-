d6713ce80735600abfe773e162b2c6e902363364
hyunnn24 <snow3515@naver.com>
2024-06-17 02:40:14 +0000

    배고파

--- app.py
+++ app.py
@@ -3,6 +3,29 @@ import openai
 import requests
 from bs4 import BeautifulSoup
 from openai import OpenAI
+import time
+
+st.title('LoL Counter Pick Recommender')
+
+enemy_champion1 = st.text_input('Enter the first enemy champion:')
+enemy_champion2 = st.text_input('Enter the second enemy champion:')
+
+def run_and_wait(client, assistant, thread):
+  run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant.id
+  )
+  while True:
+    run_check = client.beta.threads.runs.retrieve(
+      thread_id=thread.id,
+      run_id=run.id
+    )
+    print(run_check.status)
+    if run_check.status in ['queued','in_progress']:
+      time.sleep(2)
+    else:
+      break
+  return run
 
 api_key = st.text_input('Enter your OpenAI API key:', type='password')
 client = OpenAI(api_key=api_key)
@@ -24,4 +47,32 @@ download_and_save(url, filename)
 with open(filename) as fi:
   text = fi.read()
 
-st.write(text)
+#st.write(text) #TEST 
+
+my_file = client.files.create(
+    file = open(filename,'rb'),
+    purpose='assistants'
+)
+
+assistant = client.beta.assistants.create(
+  instructions="당신은 리그오브레전드 전문가 입니다 첨부파일의 정보를 이용해 카운터 챔피언을 알려주세요.",
+  model="gpt-4o",
+  tools=[{"type": "retrieval"}],
+  file_ids=[my_file.id]
+)
+
+query = f"{enemy_champion1}, {enemy_champion2}"
+
+thread = client.beta.threads.create(
+  messages=[
+    {
+      "role": "user",
+      "content": query,
+      #"file_ids": [my_file.id]
+    }
+  ]
+)
+run_and_wait(client, assistant, thread)
+
+for msg in thread_messages.data:
+  st.write(f"{msg.role}: {msg.content[0].text.value}")
