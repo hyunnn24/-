7cea16993e9b557e851fa4250ac9416db671c4ba
hyunnn24 <snow3515@naver.com>
2024-06-17 01:22:58 +0000

    프롬프트 수정444

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def call_openai_api(query, context, api_key):
     openai.api_key = api_key
     if context:
         messages = [
-            {"role": "system", "content": "문서를보고 입력 챔피언의 카운터알려줘 ."},
+            {"role": "system", "content": "문서를 기반으로 입력 챔피언의 카운터알려줘 ."},
             {"role": "user", "content": f"Context: {context}\n\nQuery: {query}"}
         ]
     else:
         messages = [
-            {"role": "system", "content": "문서를보고 입력 챔피언의 카운터알려줘 ."},
+            {"role": "system", "content": "문서를 기반으로 입력 챔피언의 카운터알려줘 ."},
             {"role": "user", "content": f"Query: {query}"}
         ]
     
